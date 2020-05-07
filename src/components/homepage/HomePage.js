670221f4c329ef516f210ac99e99f89dbd4329f5
hakokoha <kriskosi123@gmail.com>
2020-05-07 13:32:00 +0300

    Main Page Styling

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,50 +1,66 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./HomePage.module.css";
 
 class HomePage extends Component {
-    render() {
-        const { auth, tshirts } = this.props;
-        console.log('props', this.props);
-        console.log('tshirts', tshirts);
-        let tshirtsToDisplay = null;
-        if(tshirts) {
-            tshirtsToDisplay = tshirts.filter(tshirt => tshirt.id[tshirt.id.length-1] === 'M');
-        }
-
-        return (
-            <div className="homepage container">
-                <div className="row">
-                    {tshirtsToDisplay && tshirtsToDisplay.map(tshirt => {
-                        return (
-                            <Link to={'/tshirts/' + tshirt.id} key={tshirt.id}>
-                                <div className="col s12 m2 offset-m1 one-tshirt hoverable">
-                                    <img src={tshirt.imgUrl} />
-                                    <p className="tshirt-name">{tshirt.name}</p>
-                                    <p className="tshirt-price right">{tshirt.price}лв</p>
-                                </div>
-                            </Link>
-                        )
-                    })}
-                </div>
-            </div>
-        )
+  render() {
+    const { auth, tshirts } = this.props;
+    console.log("props", this.props);
+    console.log("tshirts", tshirts);
+    let tshirtsToDisplay = null;
+    if (tshirts) {
+      tshirtsToDisplay = tshirts.filter(
+        (tshirt) => tshirt.id[tshirt.id.length - 1] === "M"
+      );
     }
+
+    return (
+      <div className={styles.Items}>
+        {tshirtsToDisplay &&
+          tshirtsToDisplay.map((tshirt) => {
+            return (
+              <div className={styles.Item} key={tshirt.id}>
+                <Link
+                  to={"/tshirts/" + tshirt.id}
+                  className={styles.ImgContainer}
+                >
+                  <img src={tshirt.imgUrl} className={styles.ItemPicture} />
+                </Link>
+                <Link
+                  to={"/tshirts/" + tshirt.id}
+                  className={styles.NameContainer}
+                >
+                  <p className={styles.ItemName}>{tshirt.name}</p>
+                </Link>
+                <p className={styles.ItemPrice}>{tshirt.price}лв</p>
+                <div className={styles.RatingsAndSold}>
+                  <FontAwesomeIcon icon={faStar} className={styles.Star} />
+                  4.5
+                  <p></p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    console.log('redux bazata', state);
-    return {
-        auth: state.firebase.auth,
-        tshirts: state.firestore.ordered.tshirts
-    }
-}
+  console.log("redux bazata", state);
+  return {
+    auth: state.firebase.auth,
+    tshirts: state.firestore.ordered.tshirts,
+  };
+};
 
 export default compose(
-    connect(mapStateToProps),
-    firestoreConnect([
-        { collection: 'tshirts'  }
-    ])
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "tshirts" }])
 )(HomePage);
